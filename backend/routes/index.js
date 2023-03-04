9661887fcd378ecb6d5506cbc266e8c5dabc67a0
Aurora Ignacio <bellaignacio10@gmail.com>
2023-03-04 14:08:19 -0800

    added an api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
+
 // router.get('/hello/world', function(req, res) {
 //     // sets a cookie on the server response with the name of XSRF-TOKEN to the value of the req.csrfToken methods's return
 //     res.cookie('XSRF-TOKEN', req.csrfToken());

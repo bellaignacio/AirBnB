9bfed68a499b539c9c8810a7099f47c1ab1ce19c
Aurora Ignacio <bellaignacio10@gmail.com>
2023-03-04 13:50:55 -0800

    added route handler for CSRF token access

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-    // sets a cookie on the server response with the name of XSRF-TOKEN to the value of the req.csrfToken methods's return
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
+// router.get('/hello/world', function(req, res) {
+//     // sets a cookie on the server response with the name of XSRF-TOKEN to the value of the req.csrfToken methods's return
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
 });
 
 module.exports = router;

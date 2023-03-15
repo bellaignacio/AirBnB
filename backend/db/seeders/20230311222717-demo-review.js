77b92760ccb5219180ebee99526f8f9751631659
Aurora Ignacio <bellaignacio10@gmail.com>
2023-03-15 13:26:17 -0700

    added more review and spotImage seed data

--- backend/db/seeders/20230311222717-demo-review.js
+++ backend/db/seeders/20230311222717-demo-review.js
@@ -26,16 +26,34 @@ module.exports = {
         stars: 1
       },
       {
+        userId: 3,
+        spotId: 2,
+        review: 'Whatever experience, could be better',
+        stars: 2
+      },
+      {
         userId: 2,
         spotId: 3,
         review: 'Biggest (bungalow) bang for your buck, so worth it',
         stars: 5
       },
       {
+        userId: 1,
+        spotId: 3,
+        review: 'Almost better than my own place!',
+        stars: 4
+      },
+      {
         userId: 3,
         spotId: 1,
         review: 'Expensive townhome, but it was fun for the boys',
         stars: 3
+      },
+      {
+        userId: 2,
+        spotId: 1,
+        review: 'Cool place, perfect spot',
+        stars: 4
       }
     ], {});
   },
@@ -50,7 +68,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.in]: [1, 2, 3] },
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6] },
       // review: { [Op.in]: ['This mansion was the worst ever! So expensive..', 'Biggest (bungalow) bang for your buck, so worth it', 'Expensive townhome, but it was fun for the boys'] },
     }, {});
   }
--- backend/db/seeders/20230312215630-demo-spotImage.js
+++ backend/db/seeders/20230312215630-demo-spotImage.js
@@ -25,14 +25,29 @@ module.exports = {
         preview: true
       },
       {
+        spotId: 1,
+        url:'www.example.com/alt-demo-crib.png',
+        preview: false
+      },
+      {
         spotId: 2,
         url: 'www.example.com/one-mansion.png',
         preview: true
       },
       {
+        spotId: 2,
+        url: 'www.example.com/alt-one-mansion.png',
+        preview: false
+      },
+      {
         spotId: 3,
         url: 'www.example.com/two-bungalow.png',
         preview: true
+      },
+      {
+        spotId: 3,
+        url: 'www.example.com/alt-two-bungalow.png',
+        preview: false
       }
     ], {});
   },
@@ -47,7 +62,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.in]: [1, 2, 3] },
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6] },
     }, {});
   }
 };

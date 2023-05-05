9acf4f3dec49f03108f18cac39056e6a5fec93e5
Aurora Ignacio <bellaignacio10@gmail.com>
2023-05-04 21:01:03 -0700

    fixed bug where no spotimages in spot

--- frontend/src/components/UpdateSpotForm/index.js
+++ frontend/src/components/UpdateSpotForm/index.js
@@ -16,14 +16,14 @@ function UpdateSpotForm() {
     }, [dispatch]);
 
     if (isLoaded) {
-        const previewImages = spot.SpotImages.filter(imgObj => imgObj.preview === true);
-        const spotImages = spot.SpotImages.filter(imgObj => imgObj.preview === false);
+        const previewImages = spot.SpotImages?.filter(imgObj => imgObj.preview === true);
+        const spotImages = spot.SpotImages?.filter(imgObj => imgObj.preview === false);
 
-        if (previewImages.length) spot.previewImage = previewImages[0].url;
-        if (spotImages[0]) spot.imageOne = spotImages[0].url;
-        if (spotImages[1]) spot.imageTwo = spotImages[1].url;
-        if (spotImages[2]) spot.imageThree = spotImages[2].url;
-        if (spotImages[3]) spot.imageFour = spotImages[3].url;
+        if (previewImages?.length) spot.previewImage = previewImages[0].url;
+        if (spotImages && spotImages[0]) spot.imageOne = spotImages[0].url;
+        if (spotImages && spotImages[1]) spot.imageTwo = spotImages[1].url;
+        if (spotImages && spotImages[2]) spot.imageThree = spotImages[2].url;
+        if (spotImages && spotImages[3]) spot.imageFour = spotImages[3].url;
 
         return (
             <SpotForm spot={spot} formType="Update Spot" />

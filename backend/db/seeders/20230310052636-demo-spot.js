923277613cd681ec10347084321b86426b978eae
Aurora Ignacio <bellaignacio10@gmail.com>
2023-05-10 21:49:36 -0700

    added more user, spot, spotImage seed data

--- backend/db/seeders/20230310052636-demo-spot.js
+++ backend/db/seeders/20230310052636-demo-spot.js
@@ -19,8 +19,20 @@ module.exports = {
         lat: 53.4892340,
         lng: -103.5892058,
         name: "Demo's Crib",
-        description: 'Man cave, bachelor pad of the ages',
+        description: "Man cave, bachelor pad of the ages, Pefect for a boy's night out",
         price: 234
+      },
+      {
+        ownerId: 1,
+        address: '101 Bear Lane',
+        city: 'Big Bear',
+        state: 'California',
+        country: 'United States of America',
+        lat: -23.3349875,
+        lng: 100.2338492,
+        name: "Demo's Lakeside Cabin",
+        description: 'A woodsy getaway, perfect for dipping your toes in the lake',
+        price: 592
       },
       {
         ownerId: 2,
@@ -31,8 +43,20 @@ module.exports = {
         lat: -83.2385938,
         lng: 4.1248275,
         name: "FakeUser1's Mansion",
-        description: 'The eighth wonder of the world',
+        description: "The eighth wonder of the world, you've never seen a more beautiful house",
         price: 587
+      },
+      {
+        ownerId: 2,
+        address: '202 Destiny Road',
+        city: 'Dallas',
+        state: 'Texas',
+        country: 'United States of America',
+        lat: 34.2839283,
+        lng: 1.1829384,
+        name: "FakeUser1's Townhome",
+        description: 'Budget-friendly place for a quick weekend or business trip',
+        price: 103
       },
       {
         ownerId: 3,
@@ -43,8 +67,44 @@ module.exports = {
         lat: 49.1839583,
         lng: 133.2839500,
         name: "FakeUser2's Bungalow",
-        description: 'The hippest, most chic spot on the street',
+        description: 'The hippest, most chic spot on the street, my bungalow',
         price: 122
+      },
+      {
+        ownerId: 3,
+        address: '303 Summer Cove',
+        city: 'Miami',
+        state: 'Florida',
+        country: 'United States of America',
+        lat: -1.8939999,
+        lng: 123.1129384,
+        name: "FakeUser2's Beach House",
+        description: 'Feel the sun, sand, and breeze at this sanctuary near the shore',
+        price: 1230
+      },
+      {
+        ownerId: 4,
+        address: '444 Dummy Avenue',
+        city: 'Flagstaff',
+        state: 'Arizona',
+        country: 'United States of America',
+        lat: -50.1119204,
+        lng: 122.2937784,
+        name: "FakeUser4's RV Camping Ground",
+        description: 'The perfect spot to bring your RV or pitch a tent',
+        price: 59
+      },
+      {
+        ownerId: 4,
+        address: '404 Snowy Boulevard',
+        city: 'Brighton',
+        state: 'Utah',
+        country: 'United States of America',
+        lat: 45.2283949,
+        lng: 19.1882930,
+        name: "FakeUser4's Winter Paradise",
+        description: 'After snowboarding or skiing, relax in your own private hot tub',
+        price: 431
       }
     ], {});
   },
@@ -53,7 +113,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.in]: [1, 2, 3] },
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8] },
       // name: { [Op.in]: ["Demo's Crib", "FakeUser1's Mansion", "FakeUser2's Bungalow"] },
     }, {});
   }
--- backend/db/seeders/20230312215630-demo-spotImage.js
+++ backend/db/seeders/20230312215630-demo-spotImage.js
@@ -26,33 +26,188 @@ module.exports = {
         preview: false
       },
       {
-        spotId: 2,
-        url: 'https://i.ibb.co/3cTx57s/mansion-preview.jpg',
-        preview: true
+        spotId: 1,
+        url: 'https://i.ibb.co/9cj25Kr/man-cave-alt3.jpg',
+        preview: false
       },
       {
-        spotId: 2,
-        url: 'https://i.ibb.co/prp0ZHF/mansion-alt1.jpg',
+        spotId: 1,
+        url: 'https://i.ibb.co/F75n9vP/man-cave-alt4.jpg',
         preview: false
       },
       {
         spotId: 2,
+        url: 'https://i.ibb.co/rMsBNRB/cabin-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.ibb.co/YWptzLf/cabin-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://i.ibb.co/DPBGDJQ/cabin-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://i.ibb.co/Kj1hRNb/cabin-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://i.ibb.co/72jm3kx/cabin-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://i.ibb.co/3cTx57s/mansion-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.ibb.co/prp0ZHF/mansion-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 3,
         url: 'https://i.ibb.co/7r1w7PW/mansion-alt2.jpg',
         preview: false
       },
       {
         spotId: 3,
-        url: 'https://i.ibb.co/jJNnxpZ/bungalow-preview.jpg',
-        preview: true
-      },
-      {
-        spotId: 3,
-        url: 'https://i.ibb.co/pWhJ41f/bungalow-alt1.jpg',
+        url: 'https://i.ibb.co/6rvTvv9/mansion-alt3.jpg',
         preview: false
       },
       {
         spotId: 3,
+        url: 'https://i.ibb.co/tzHkWG5/mansion-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://i.ibb.co/ZGfwf7m/townhome-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.ibb.co/rFrbR2G/townhome-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://i.ibb.co/gWMGDJ9/townhome-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://i.ibb.co/3FGcktB/townhome-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://i.ibb.co/3vBSqQf/townhome-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://i.ibb.co/jJNnxpZ/bungalow-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.ibb.co/pWhJ41f/bungalow-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 5,
         url: 'https://i.ibb.co/f8bf8sm/bungalow-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://i.ibb.co/kGVXF1P/bungalow-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://i.ibb.co/6bMZsj8/bungalow-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://i.ibb.co/FWSLVRq/beach-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: 'https://i.ibb.co/kqVKyLc/beach-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://i.ibb.co/XZzGFdD/beach-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://i.ibb.co/5vCJxZv/beach-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://i.ibb.co/zrrTCpH/beach-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: 'https://i.ibb.co/C2s1KJV/rv-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: 'https://i.ibb.co/fGtDs6Q/rv-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: 'https://i.ibb.co/WHt0fX0/rv-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: 'https://i.ibb.co/m6cBcwh/rv-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: 'https://i.ibb.co/23qw4L1/rv-alt4.jpg',
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: 'https://i.ibb.co/djNBRLT/winter-preview.jpg',
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: 'https://i.ibb.co/8NRp2fz/winter-alt1.jpg',
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: 'https://i.ibb.co/dMrvFhs/winter-alt2.jpg',
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: 'https://i.ibb.co/CH5qRFF/winter-alt3.jpg',
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: 'https://i.ibb.co/Z16zL2q/winter-alt4.jpg',
         preview: false
       }
     ], {});
@@ -62,7 +217,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40] },
     }, {});
   }
 };
